a3d65f37d15d54b50f5111f34ea371a7407a2ce1
Michael Pfohl <rmpfohl@gmail.com>
2020-08-25 22:05:50 -0500

    local storage fix, need to adjust button events

--- javascripts/helpers / data/dinos.js
+++ javascripts/helpers / data/dinos.js
@@ -1,17 +1,28 @@
 const dinos = [];
 
-// const getDinos = () => {
-//     return dinos;
-// }
+const getDinos = () => {
+    return dinos;
+}
+
+const setDinos = (value) => {
+    dinos.push(value);
+}
 
 const sendToLocal = (key, value) => {
-    let passedValue = JSON.stringify(value);
-    localStorage.setItem(key, passedValue);
-  }
+    setDinos(value);
+    let passedArray = JSON.stringify(getDinos());
+    localStorage.setItem(key, passedArray);
+}
   
 const getFromLocal = (key) => {
     let retrievedValue = localStorage.getItem(key);
-    return JSON.parse(retrievedValue);
-  }
+    console.log(retrievedValue);
+    if (retrievedValue === null){
+        return [];
+    } else {
+        console.log(JSON.parse(retrievedValue));
+        return JSON.parse(retrievedValue);
+    }
+}
 
 export { sendToLocal, getFromLocal }
